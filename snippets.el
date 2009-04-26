1469404b46bc9555c3591d3e92e09a0a18f8de44
Markus Knittig <markus@myd0.de>
2009-04-26 22:27:20 +0200

    Added yasnippets-rspec as git submodule

--- snippets.el
+++ snippets.el
@@ -1,4 +1,4 @@
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
-;(yas/load-directory "~/.emacs.d/vendor/yasnippets-rspec/rspec-snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippets-rspec/rspec-snippets")
