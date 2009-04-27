0873015f1bf53a942357f12b58d656f2944b3529
Markus Knittig <markus@myd0.de>
2009-04-27 12:44:36 +0200

    Don't expand snippets in strings and comments

--- snippets.el
+++ snippets.el
@@ -3,3 +3,10 @@
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
 (yas/load-directory "~/.emacs.d/vendor/yasnippets-rspec/rspec-snippets")
 (yas/load-directory "~/.emacs.d/vendor/yasnippets-rspec/rails-snippets")
+
+(setq yas/buffer-local-condition
+      '(or (not (or (string= "font-lock-comment-face"
+                             (get-char-property (point) 'face))
+                    (string= "font-lock-string-face"
+                             (get-char-property (point) 'face))))
+           '(require-snippet-condition . force-in-comment)))
