6d8b661f64d61e5fe42acc43cb220ebb74e47f97
Markus Knittig <markus@myd0.de>
2009-05-01 23:13:08 +0200

    Added apache-mode configuration

--- modes.el
+++ modes.el
@@ -1,2 +1,11 @@
 ;; highlight FIXME, TODO, etc.
 (require 'fixme-mode)
+
+;; apache-mode
+(autoload 'apache-mode "apache-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
+(add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
+(add-to-list 'auto-mode-alist '("apache2?\\.conf\\'"    . apache-mode))
+(add-to-list 'auto-mode-alist '("ports\\.conf\\'" . apache-mode))
+(add-to-list 'auto-mode-alist '("apache2?/conf.d/" . apache-mode))
+(add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
